72e85f17b42092add7994cc261829e5ee80b4a7e
Alibaba <abdulmanafaliu414@gmail.com>
2024-05-24 19:47:38 +0000

    A lot of changes made

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./App.css"
+import Image_person from '../public/assets/images/image-avatar.webp'
+
+function App() {
+  return (
+    <>
+    
+    <div className='card-container'>
+      <div>
+      <div>
+      <img src="./public/assets/images/illustration-article.svg" alt="" />
+      </div>
+      
+      <div className='button'>
+        <button style={{fontWeight:'500'}}>Learning</button>
+      </div>
+     <div>
+      <p>Published 21 Dec 2023</p>
+      <p className='course' style={{fontWeight: '900'}}>HTML & CSS foundations</p>
+      <p> These languages are the backbone of every <br /> website, defining structure, content, and <br /> presentation.</p>
+     </div>
+     <div className='last-container'>
+      <img src={Image_person} alt="" />
+      <p>Greg Hooper</p>
+     </div>
+      </div>
+    </div>
+
+    </>
+    )
+}
+
+export default App
